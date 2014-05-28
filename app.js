2af4d699685460d107cc550723d02e6a35605eb1
pjwl33 <paul.leejw@gmail.com>
2014-05-28 12:31:15 -0400

    more fxns

--- app.js
+++ app.js
@@ -28,22 +28,6 @@ $(document).ready(function() {
         "background-position": $(event.target).scrollLeft()/6-100+ "px 0"
       });
     },
-
-    handleNavClick: function(event, el) {
-      event.preventDefault();
-      var position = $(el).attr("href").split("-").pop();
-
-      this.el.slider.animate({
-        scrollLeft: position * this.slideWidth
-      }, this.timing);
-
-      this.changeActiveNav(el);
-    },
-
-    changeActiveNav: function(el) {
-      this.el.allNavButtons.removeClass("active");
-      $(el).addClass("active");
-    }
   };
 
   slider.init();
