b97b45804e7834ed03cf70237453702f8b05218c
pjwl33 <paul.leejw@gmail.com>
2014-05-29 01:50:24 -0400

    woop

--- app.js
+++ app.js
@@ -2,9 +2,9 @@ $(document).ready(function() {
   $('#about-wrap').hide();
   $('#project-wrap').hide();
   $('#contact-wrap').hide();
-  $('#proj1').hide();
-  $('#proj2').hide();
-  $('#proj3').hide();
+  // $('#proj1').hide();
+  // $('#proj2').hide();
+  // $('#proj3').hide();
   $('a').attr('target', '_blank');
 });
 
@@ -12,9 +12,6 @@ function showDiv(div) {
   var aWrap = $('#about-wrap');
   var pWrap = $('#project-wrap');
   var cWrap = $('#contact-wrap');
-  var p1Info = $('#proj1');
-  var p2Info = $('#proj2');
-  var p3Info = $('#proj3');
   var pic = $('.pic-wrap');
 
   if (div == 'top') {
@@ -33,9 +30,6 @@ function showDiv(div) {
     $('#' + div).toggle(175);
   }
   if (div == 'project-wrap') {
-    p1Info.hide();
-    p2Info.hide();
-    p3Info.hide();
     aWrap.hide();
     cWrap.hide();
     $('#' + div).toggle(175);
@@ -44,21 +38,6 @@ function showDiv(div) {
     pWrap.hide();
     aWrap.hide();
     $('#' + div).toggle(175);
-  }
-  if (div == 'proj1') {
-    p2Info.hide();
-    p3Info.hide();
-    p1Info.toggle(150);
-  }
-  if (div == 'proj2') {
-    p1Info.hide();
-    p3Info.hide();
-    p2Info.toggle(150);
-  }
-  if (div == 'proj3') {
-    p2Info.hide();
-    p1Info.hide();
-    p3Info.toggle(150);
   }
   $('html, body').animate({scrollTop: $(document).height()}, 'slow');
 }
