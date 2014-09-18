c588107365777c5ef397efacfc69996b4192a885
pjwl33 <paul.leejw@gmail.com>
2014-09-17 22:22:22 -0400

    some changes

--- app.js
+++ app.js
@@ -1,46 +1,15 @@
 $(document).ready(function() {
-  $('#about-wrap').hide();
-  $('#project-wrap').hide();
-  $('#contact-wrap').hide();
-
   var first = $('#about-pic-1').attr('src');
   var second = $('#about-pic-2').attr('src');
   var third = $('#about-pic-3').attr('src');
   var fourth = $('#about-pic-4').attr('src');
   var fifth = $('#about-pic-5').attr('src');
   var imgArray = [first, second, third, fourth, fifth];
+
   picChanger(imgArray);
   setInterval(function(){ picChanger(imgArray); }, 8000);
+  startAngular();
 });
-
-function showDiv(div) {
-  var aWrap = $('#about-wrap');
-  var pWrap = $('#project-wrap');
-  var cWrap = $('#contact-wrap');
-  if (div == 'top') {
-    aWrap.hide();
-    pWrap.hide();
-    cWrap.hide();
-  }
-  if (div == 'about-wrap') {
-    pWrap.hide();
-    cWrap.hide();
-    $('#' + div).slideToggle(200);
-    $('body').animate({scrollTop: 0}, 'fast');
-  }
-  if (div == 'project-wrap') {
-    aWrap.hide();
-    cWrap.hide();
-    $('#' + div).slideToggle(200);
-    $('body').animate({scrollTop: 235}, 'fast');
-  }
-  if (div == 'contact-wrap') {
-    pWrap.hide();
-    aWrap.hide();
-    $('#' + div).slideToggle(200);
-    $('body').animate({scrollTop: $(document).height()}, 'fast');
-  }
-}
 
 function picChanger(imgArray) {
   var aboutPic = $('#about-pic');
@@ -50,4 +19,14 @@ function picChanger(imgArray) {
     $(this).attr('src', img);
     $(this).fadeIn(700);
   });
+}
+
+function startAngular(){
+  var ang = angular.module('ang', ['ui.bootstrap']);
+
+  ang.controller('MyController', ['$scope', '$animate', function($scope, $animate){
+    $scope.showDiv = function(divType){
+      $scope.showType = divType;
+    };
+  }]);
 }
