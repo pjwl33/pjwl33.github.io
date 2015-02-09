0563b0bc6e1d0c650182e698ae5980a33e9e5972
pjwl33 <paul.leejw@gmail.com>
2015-02-09 16:13:24 -0500

    testing camera

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ $(document).ready(function() {
   picChanger(imgArray);
   setInterval(function(){ picChanger(imgArray); }, 8000);
 
-  // backgroundVideoStart();
+  startCamera();
   startAngular();
 });
 
@@ -23,30 +23,70 @@ function picChanger(imgArray) {
   });
 }
 
-// function backgroundVideoStart() {
-//   var v = document.getElementById('v');
-//   var canvas = document.getElementById('c');
-//   var context = canvas.getContext('2d');
+function startCamera() {
+  var v = document.getElementById('v');
+  var canvas = document.getElementById('c');
+  var context = canvas.getContext('2d');
+  var save = document.getElementById('save-pic');
 
-//   var cw = Math.floor(canvas.clientWidth / 100);
-//   var ch = Math.floor(canvas.clientHeight / 100);
-//   canvas.width = cw;
-//   canvas.height = ch;
+  var cw = canvas.clientWidth;
+  var ch = canvas.clientHeight;
+  canvas.width = cw;
+  canvas.height = ch;
 
-//   v.addEventListener('play', function() {
-//     draw(this, context, cw, ch);
-//   }, false);
+  navigator.getUserMedia = (navigator.getUserMedia ||
+    navigator.webkitGetUserMedia ||
+    navigator.mozGetUserMedia ||
+    navigator.msGetUserMedia);
+  if (navigator.getUserMedia) {
+    navigator.getUserMedia({
+        video: true,
+        audio: false
+      },
+      function(stream) {
+        var url = window.URL || window.webkitURL;
+        v.src = url ? url.createObjectURL(stream) : stream;
+        v.play();
 
-//   c.addEventListener('')
+        v.addEventListener('play', function() {
+          draw(this, context, cw, ch);
+        }, false);
 
-//   function draw(v, c, w, h) {
-//     if (v.paused || v.ended) return false;
-//     c.drawImage(v, 0, 0, w, h);
-//     setTimeout(draw, 20, v, c, w, h);
-//   }
+        function draw(v, c, w, h) {
+          if (v.paused || v.ended) return false;
+          var image, data, i, r, g, b, brightness;
 
-//   v.play();
-// }
+          c.drawImage(v, 0, 0, w, h);
+
+          image = c.getImageData(0, 0, w, h);
+          data = image.data;
+          // array of pixels
+          for (i = 0; i < data.length; i += 4) {
+            r = data[i] * 2;
+            g = data[i + 1] * 2;
+            b = data[i + 2] * 2;
+            brightness = (r + b + g) / 3;
+
+            data[i] = data[i + 1] = data[i + 2] = brightness;
+          }
+
+          image.data = data;
+
+          c.putImageData(image, 0, 0);
+
+          setTimeout(draw, 20, v, c, w, h);
+        }
+      },
+      function(error) {
+        alert('Something went wrong. (error code ' + error.code + ')');
+        return;
+      }
+    );
+  } else {
+    alert('Sorry, the browser you are using doesn\'t support getUserMedia');
+    return;
+  }
+}
 
 function startAngular(){
   var angApp = angular.module('myApp', []);
