29f61e2f5015b63c17899c501f98f068809b1548
pjwl33 <paul.leejw@gmail.com>
2015-02-12 11:36:26 -0500

    ya know

--- js/app.js
+++ js/app.js
@@ -10,7 +10,7 @@ $(document).ready(function() {
   // setInterval(function(){ picChanger(imgArray); }, 8000);
 
   // startCamera();
-  startAngular();
+  // startAngular();
 });
 
 function picChanger(imgArray) {
@@ -23,66 +23,66 @@ function picChanger(imgArray) {
   });
 }
 
-function startAngular(){
-  var angApp = angular.module('myApp', []);
+// function startAngular(){
+//   var angApp = angular.module('myApp', []);
 
-  var myProjects = [
-    {
-      url: "http://nyfi.herokuapp.com/",
-      title: "NYfi",
-      image: "images/nyfi.png",
-      description: "First Rails application: search app for NYC's wifi hotspots and their Yelp ratings.",
-      source: "https://github.com/pjwl33/NYfi"
-    },
-    {
-      url: "http://ratemylunch.herokuapp.com/",
-      title: "Rate My Lunch",
-      image: "images/ratemylunch.png",
-      description: "A fun and innovative game for Employees to inspire co-workers to eat the best lunches for their health needs. Hack//Dining NYC Hackathon Project.",
-      source: "https://github.com/pjwl33/lunch_it"
-    },
-    {
-      url: "http://emochat.herokuapp.com/",
-      title: "Emochat",
-      image: "images/emochat.png",
-      description: "Chat app with changing avatar icons according to user hashtag and self-taken photos.",
-      source: "https://github.com/originalblend337/chat_app"
-    },
-    {
-      url: "http://groupprov.herokuapp.com/",
-      title: "Group Prov",
-      image: "images/groupprov.png",
-      description: "Music playing app where users can record, play, and loop tracks with others using their QWERTY keyboards. Simulating MIDI playback functionality.",
-      source: "https://github.com/pjwl33/Group.pRov"
-    },
-    // {
-    //   url: "http://hatetolove.herokuapp.com/",
-    //   title: "HateToLove",
-    //   image: "images/hatetolove.jpg",
-    //   description: "Rails application (iOS version in the works) where users can find the loves of their lives by hating the same things.",
-    //   source: "https://github.com/pjwl33/date_app"
-    // },
-    {
-      url: "http://www.lolseriousbiz.com",
-      title: "serious biz",
-      image: "images/lolseriousbiz.png",
-      description: "Angular/Rails application connected with Riot's League of Legends API. Also a fan-site for my teammates from LCS Season 4.",
-      source: "https://github.com/pjwl33/lol_app"
-    },
-    {
-      url: "http://www.leedentalgroup.net",
-      title: "Lee Dental Group",
-      image: "images/leedentalgroup.png",
-      description: "Freelance Project for 'Lee Dental Group', a family-oriented dental clinic in Michigan. Built with Angular.js, Angular-Parallax, Boostrap, and Love.",
-      source: "https://github.com/leedentalgroup/leedentalgroup.github.io"
-    },
-  ];
+//   var myProjects = [
+//     {
+//       url: "http://nyfi.herokuapp.com/",
+//       title: "NYfi",
+//       image: "images/nyfi.png",
+//       description: "First Rails application: search app for NYC's wifi hotspots and their Yelp ratings.",
+//       source: "https://github.com/pjwl33/NYfi"
+//     },
+//     {
+//       url: "http://ratemylunch.herokuapp.com/",
+//       title: "Rate My Lunch",
+//       image: "images/ratemylunch.png",
+//       description: "A fun and innovative game for Employees to inspire co-workers to eat the best lunches for their health needs. Hack//Dining NYC Hackathon Project.",
+//       source: "https://github.com/pjwl33/lunch_it"
+//     },
+//     {
+//       url: "http://emochat.herokuapp.com/",
+//       title: "Emochat",
+//       image: "images/emochat.png",
+//       description: "Chat app with changing avatar icons according to user hashtag and self-taken photos.",
+//       source: "https://github.com/originalblend337/chat_app"
+//     },
+//     {
+//       url: "http://groupprov.herokuapp.com/",
+//       title: "Group Prov",
+//       image: "images/groupprov.png",
+//       description: "Music playing app where users can record, play, and loop tracks with others using their QWERTY keyboards. Simulating MIDI playback functionality.",
+//       source: "https://github.com/pjwl33/Group.pRov"
+//     },
+//     // {
+//     //   url: "http://hatetolove.herokuapp.com/",
+//     //   title: "HateToLove",
+//     //   image: "images/hatetolove.jpg",
+//     //   description: "Rails application (iOS version in the works) where users can find the loves of their lives by hating the same things.",
+//     //   source: "https://github.com/pjwl33/date_app"
+//     // },
+//     {
+//       url: "http://www.lolseriousbiz.com",
+//       title: "serious biz",
+//       image: "images/lolseriousbiz.png",
+//       description: "Angular/Rails application connected with Riot's League of Legends API. Also a fan-site for my teammates from LCS Season 4.",
+//       source: "https://github.com/pjwl33/lol_app"
+//     },
+//     {
+//       url: "http://www.leedentalgroup.net",
+//       title: "Lee Dental Group",
+//       image: "images/leedentalgroup.png",
+//       description: "Freelance Project for 'Lee Dental Group', a family-oriented dental clinic in Michigan. Built with Angular.js, Angular-Parallax, Boostrap, and Love.",
+//       source: "https://github.com/leedentalgroup/leedentalgroup.github.io"
+//     },
+//   ];
 
-  angApp.controller('MyController', function($scope){
-    $scope.showType = false;
-    $scope.projects = myProjects;
-    $scope.showDiv = function(divType){
-      $scope.showType = $scope.showType == divType? false : divType;
-    };
-  });
-}
+//   angApp.controller('MyController', function($scope){
+//     $scope.showType = false;
+//     $scope.projects = myProjects;
+//     $scope.showDiv = function(divType){
+//       $scope.showType = $scope.showType == divType? false : divType;
+//     };
+//   });
+// }
