b87cd7a3f1870e7536e18aa55138c4021ca781bf
pjwl33 <paul.leejw@gmail.com>
2016-08-17 10:52:51 -0400

    options filled

--- js/bingo.js
+++ js/bingo.js
@@ -4,19 +4,20 @@ $(document).ready(function() {
   var options = [
     { type: "Free Space", state: false },
     { type: "You People", state: false },
-    { type: "Where are you from", state: false },
+    { type: "Payam ISIS, Rohin Black, Asians mixed up", state: false },
     { type: "Where are you actually from", state: false },
-    { type: "Something in a foreign language", state: false },
+    { type: "Try to say something in foreign language", state: false },
     { type: "Comparison to a famous minority", state: false },
     { type: "Racial slang/word", state: false },
-    { type: "Free Space", state: false },
-    { type: "Free Space", state: false },
+    { type: "Confederate Flag", state: false },
+    { type: "Jesus Lecture", state: false },
   ];
 
   if (squares) {
     squares = JSON.parse(squares);
   } else {
     squares = _.shuffle(options);
+    localStorage.setItem('squares', JSON.stringify(squares));
   }
 
 
