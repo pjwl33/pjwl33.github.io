7501d8f7ab27902b8e6eb303a568905e34e91ef6
pjwl33 <paul.leejw@gmail.com>
2016-08-31 10:48:55 -0400

    bingo disabled

--- js/bingo.js
+++ js/bingo.js
@@ -1,68 +1,68 @@
-var squares = localStorage.getItem('squares');
-var options = [
-  { type: 'Show up to the wedding (Free Space)', state: true, index: 0 },
-  { type: '"You People"', state: false, index: 1 },
-  { type: 'Payam related to ISIS?', state: false, index: 2 },
-  { type: 'Tries to guess your ethnicity', state: false, index: 3 },
-  { type: '"Where are you actually from?"', state: false, index: 4 },
-  { type: 'Asks you to say something in your language', state: false, index: 5 },
-  { type: "Tells you they love your people's food", state: false, index: 6 },
-  { type: 'Racial slang/slur', state: false, index: 7 },
-  { type: 'Confederate Flag', state: false, index: 8 },
-  { type: 'Jesus Lecture', state: false, index: 9 },
-  { type: "Spot another minority you don't know", state: false, index: 10 },
-  { type: 'Rohin takes off his shirt', state: false, index: 11 },
-  { type: 'Rohin is black?', state: false, index: 12 },
-  { type: 'Asian Mixup?', state: false, index: 13 },
-  { type: 'Drink Moonshine', state: false, index: 14 },
-  { type: 'Tells you about another person of your race', state: false, index: 15 },
-];
+// var squares = localStorage.getItem('squares');
+// var options = [
+//   { type: 'Show up to the wedding (Free Space)', state: true, index: 0 },
+//   { type: '"You People"', state: false, index: 1 },
+//   { type: 'Payam related to ISIS?', state: false, index: 2 },
+//   { type: 'Tries to guess your ethnicity', state: false, index: 3 },
+//   { type: '"Where are you actually from?"', state: false, index: 4 },
+//   { type: 'Asks you to say something in your language', state: false, index: 5 },
+//   { type: "Tells you they love your people's food", state: false, index: 6 },
+//   { type: 'Racial slang/slur', state: false, index: 7 },
+//   { type: 'Confederate Flag', state: false, index: 8 },
+//   { type: 'Jesus Lecture', state: false, index: 9 },
+//   { type: "Spot another minority you don't know", state: false, index: 10 },
+//   { type: 'Rohin takes off his shirt', state: false, index: 11 },
+//   { type: 'Rohin is black?', state: false, index: 12 },
+//   { type: 'Asian Mixup?', state: false, index: 13 },
+//   { type: 'Drink Moonshine', state: false, index: 14 },
+//   { type: 'Tells you about another person of your race', state: false, index: 15 },
+// ];
 
-$(document).ready(function() {
+// $(document).ready(function() {
 
-  if (squares) {
-    squares = JSON.parse(squares);
-  } else {
-    squares = _.shuffle(options);
-    localStorage.setItem('squares', JSON.stringify(squares));
-  }
+//   if (squares) {
+//     squares = JSON.parse(squares);
+//   } else {
+//     squares = _.shuffle(options);
+//     localStorage.setItem('squares', JSON.stringify(squares));
+//   }
 
-  _.each(squares, function(s) {
-    var box = $('<div>');
-    box.addClass('bingo-box');
-    box.attr('id', s.index);
-    box.attr('onclick', 'toggleSquare(' + s.index + ')');
+//   _.each(squares, function(s) {
+//     var box = $('<div>');
+//     box.addClass('bingo-box');
+//     box.attr('id', s.index);
+//     box.attr('onclick', 'toggleSquare(' + s.index + ')');
 
-    var text = $('<p>');
-    text.text(s.type);
-    box.append(text);
+//     var text = $('<p>');
+//     text.text(s.type);
+//     box.append(text);
 
-    if (s.state) {
-      box.addClass('on');
-    } else {
-      box.removeClass('on');
-    }
-    $('.bingo-board').append(box);
-  });
+//     if (s.state) {
+//       box.addClass('on');
+//     } else {
+//       box.removeClass('on');
+//     }
+//     $('.bingo-board').append(box);
+//   });
 
-});
+// });
 
-function resetBoard() {
-  var warning = confirm('This will completely RESET your bingo board.');
+// function resetBoard() {
+//   var warning = confirm('This will completely RESET your bingo board.');
 
-  if (warning) {
-    localStorage.clear();
-    location.reload();
-  }
-}
+//   if (warning) {
+//     localStorage.clear();
+//     location.reload();
+//   }
+// }
 
-function toggleSquare(index) {
-  var selectedBox = $('div#' + index);
-  console.log(selectedBox);
-  selectedBox.toggleClass('on');
+// function toggleSquare(index) {
+//   var selectedBox = $('div#' + index);
+//   console.log(selectedBox);
+//   selectedBox.toggleClass('on');
 
-  _.each(squares, function(s) {
-    if (s.index == index) s.state = !s.state;
-    localStorage.setItem('squares', JSON.stringify(squares));
-  });
-}
+//   _.each(squares, function(s) {
+//     if (s.index == index) s.state = !s.state;
+//     localStorage.setItem('squares', JSON.stringify(squares));
+//   });
+// }
